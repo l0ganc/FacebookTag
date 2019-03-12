1dbbecb57bbba6f77a4bd2c7b597aed998948fb0
l0ganc <jingyige@usc.edu>
2019-03-12 15:01:16 -0700

    Initial commit

---
+++ src/main/java/LC125ValidPalindrome.java
@@ -0,0 +1,39 @@
+public class LC125ValidPalindrome {
+    /**
+     * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+     *
+     * Note: For the purpose of this problem, we define empty string as valid palindrome.
+     *
+     * Example 1:
+     *
+     * Input: "A man, a plan, a canal: Panama"
+     * Output: true
+     *
+     * time = O(n)
+     * space = O(1)
+     */
+
+    public static boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        s = s.toLowerCase();
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j++;
+            }
+
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
