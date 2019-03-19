e2afc295e4f56834e84a804a9ba632ee26a4aa0e
l0ganc <jingyige@usc.edu>
2019-03-19 16:04:59 -0700

    fix a small error in LC560

--- src/main/java/LC560SubarraySumEqualsK.java
+++ src/main/java/LC560SubarraySumEqualsK.java
@@ -5,7 +5,7 @@ public class LC560SubarraySumEqualsK {
     // method 1: O(n^2) and O(1) space
     public int subarraySum1(int[] nums, int k) {
         int count = 0;
-        for (int start = 0; start < nums.length; i++) {
+        for (int start = 0; start < nums.length; start++) {
             int sum =  0;
             for (int end = start; end < nums.length; end++) {
                 sum += nums[end];
