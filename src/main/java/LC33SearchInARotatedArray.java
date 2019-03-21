df82858d68d959469f4219948b99a723ce0d7fd7
l0ganc <jingyige@usc.edu>
2019-03-21 10:42:39 -0700

    LC33 search in a rotated array

---
+++ src/main/java/LC33SearchInARotatedArray.java
@@ -0,0 +1,40 @@
+public class LC33SearchInARotatedArray {
+    /**
+     * Input: nums = [4,5,6,7,0,1,2], target = 0
+     * Output: 4
+     *
+     * binary search
+     *
+     * time: O(logn) space:  O(1)
+     */
+
+    // 不带重复元素
+    public static int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
