4689a4c64416a3ca917084a1d89cbc921a723221
l0ganc <jingyige@usc.edu>
2019-03-20 19:46:38 -0700

    LC15 3Sum

---
+++ src/main/java/LC15_3Sum.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LC15_3Sum {
+    /**
+     * time = O(N^2)
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int low = i + 1;
+            int high = nums.length - 1;
+            int target = 0 - nums[i];
+            while (low < high) {
+                if (nums[low] + nums[high] == target) {
+                    res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    while (low < high && nums[low] == nums[low + 1]) {
+                        low++;
+                    }
+                    while (low < high && nums[high] == nums[high - 1]) {
+                        high--;
+                    }
+                    low++;
+                    high--;
+                } else if (nums[low] + nums[high] < target) {
+                    low++;
+                } else {
+                    high--;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+}
