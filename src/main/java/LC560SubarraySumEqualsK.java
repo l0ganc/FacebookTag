073bdcc7c0e55020bc2f5dc8bad8777fc4194425
l0ganc <jingyige@usc.edu>
2019-03-23 14:49:46 -0700

    LC65 Valid Number 用了ifs判断 包含原版的以及阉割版的没有e

--- src/main/java/LC560SubarraySumEqualsK.java
+++ src/main/java/LC560SubarraySumEqualsK.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class LC560SubarraySumEqualsK {
     // method 1: O(n^2) and O(1) space
-    public int subarraySum1(int[] nums, int k) {
+    public static int subarraySum1(int[] nums, int k) {
         int count = 0;
         for (int start = 0; start < nums.length; start++) {
             int sum =  0;
@@ -21,7 +21,7 @@ public class LC560SubarraySumEqualsK {
     // find how many pairs of <i, j> where i < j and prefixSum[j] - prefixSum[i] == k
     // key : prefix sum value
     // value : number of occurrence of the prefix sum
-    public int subarraySum2(int[] nums, int k) {
+    public static int subarraySum2(int[] nums, int k) {
         int count = 0, sum = 0;
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
@@ -33,5 +33,31 @@ public class LC560SubarraySumEqualsK {
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
+    }
+
+    // method 3: 如果都是正数，空间如何O(1)，这时候用two pointer
+    public static int subarraySum3(int[] nums, int k) {
+        if (k < 0) {
+            return 0;
+        }
+
+        int i = 0, sum = 0, res = 0;
+        for (int j = 0; j < nums.length; j++) {
+            sum += nums[j];
+            while (sum > k) {
+                sum -= nums[i++];
+            }
+            if (sum == k) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 2, 1, 5, 3};
+        System.out.println(subarraySum1(nums, 6));
+        System.out.println(subarraySum2(nums, 6));
+        System.out.println(subarraySum3(nums, 6));
     }
 }
---
+++ src/main/java/LC65ValidNumber.java
@@ -0,0 +1,99 @@
+public class LC65ValidNumber {
+    /**
+     * Validate if a given string can be interpreted as a decimal number.
+     *
+     * fb常考的是不带e的，只有正负号小数，下面就给出这种情况下的解
+     */
+
+    //  简化版，不用考虑e
+    public static boolean isNumberSimple(String s) {
+        s = s.trim();  // 如果不给用trim就用一个point来skip空格
+        boolean pointSeen = false;
+        boolean numberSeen = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ('0' <= s.charAt(i) && s.charAt(i) <= '9') {
+                numberSeen = true;
+            } else if (s.charAt(i) == '.') {
+                if (pointSeen) {
+                    pointSeen = false;
+                }
+                pointSeen =  true;
+            } else if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+                if (i != 0) {
+                    return false;
+                }
+            } else {
+                return  false;
+            }
+        }
+        return numberSeen;
+    }
+
+    // LC65: Valid number原版
+    public static boolean isNumber(String s) {
+        s = s.trim();
+        boolean eSeen = false;
+        boolean pointSeen = false;
+        boolean numberSeen = false;
+        boolean numberAfterE = false;
+
+        for (int i = 0; i < s.length(); i++)  {
+            if ('0' <= s.charAt(i) && s.charAt(i) <= '9') {
+                numberSeen = true;
+                numberAfterE = true;
+            } else if (s.charAt(i) == '.') {
+                if (eSeen || pointSeen) {
+                    return false;
+                }
+                pointSeen = true;
+            } else if (s.charAt(i) == 'e') {
+                if (eSeen || !numberSeen) {
+                    return false;
+                }
+                numberAfterE = false;
+                eSeen = true;
+            } else if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+                if (i != 0 && s.charAt(i - 1) != 'e') {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return numberSeen && numberAfterE;
+    }
+
+    /**
+     * "0" => true
+     * " 0.1 " => true
+     * "abc" => false
+     * "1 a" => false
+     * "2e10" => true
+     * " -90e3   " => true
+     * " 1e" => false
+     * "e3" => false
+     * " 6e-1" => true
+     * " 99e2.5 " => false
+     * "53.5e93" => true
+     * " --6 " => false
+     * "-+3" => false
+     * "95a54e53" => false
+     */
+    public static void main(String[] args) {
+        System.out.println(isNumber("0"));
+        System.out.println(isNumber(" 0.1"));
+        System.out.println(isNumber("abc"));
+        System.out.println(isNumber("1 a"));
+        System.out.println(isNumber("2e10"));
+        System.out.println(isNumber(" -90e3"));
+        System.out.println(isNumber(" 1e"));
+        System.out.println(isNumber("e3"));
+        System.out.println(isNumber(" 6e-1"));
+        System.out.println(isNumber(" 99e2.5"));
+        System.out.println(isNumber("53.5e93"));
+        System.out.println(isNumber(" --6"));
+        System.out.println(isNumber("-+3"));
+        System.out.println(isNumber("95a54e53"));
+    }
+}
