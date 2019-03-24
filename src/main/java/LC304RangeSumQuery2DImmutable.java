212bfca5497471b832800447ba6fddea86ebb0ea
l0ganc <jingyige@usc.edu>
2019-03-24 13:31:17 -0700

    LC304 Range Sum Query 2D - Imuutable 再次深入理解dp[i][j]与matrix的对应关系

--- src/main/java/LC304RangeSumQuery2DImmutable.java
+++ src/main/java/LC304RangeSumQuery2DImmutable.java
@@ -15,13 +15,13 @@ public class LC304RangeSumQuery2DImmutable {
      * 思路：dp，前缀和相减
      */
 
-    int[][] dp;
+    static int[][] dp;
     public LC304RangeSumQuery2DImmutable(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return;
         }
 
-        // dp[i][j]是从[0][0] ~ [i][j]这范围内所有元素的和
+        // dp[i][j]是从matrix[0][0]一直加到matrix[i - 1][j - 1]这范围内所有元素的和
         dp = new int[matrix.length + 1][matrix[0].length + 1];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
@@ -30,7 +30,22 @@ public class LC304RangeSumQuery2DImmutable {
         }
     }
 
-    public int sumRegion(int row1, int col1, int row2, int col2) {
+    public  int sumRegion(int row1, int col1, int row2, int col2) {
         return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{
+                {3, 0, 1, 4, 2},
+                {5, 6, 3, 2, 1},
+                {1, 2, 0, 1, 5},
+                {4, 1, 0, 1, 7},
+                {1, 0, 3, 0, 5}
+        };
+        LC304RangeSumQuery2DImmutable demo = new LC304RangeSumQuery2DImmutable(matrix);
+
+        System.out.println(demo.sumRegion(2, 1,4,3));
+        System.out.println(demo.sumRegion(1, 1,2,2));
+        System.out.println(demo.sumRegion(1, 2,2,4));
     }
 }
