d542483b748aa61385a7022d9c1287428800f851
l0ganc <jingyige@usc.edu>
2019-03-26 19:48:04 -0700

    LC282 Expression Add  Operation

---
+++ src/main/java/LC282ExpressionAddOperators.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC282ExpressionAddOperators {
+    /**
+     * Given a string that contains only digits 0-9 and a target value,
+     * return all possibilities to add binary operators (not unary) +, -, or * between the digits
+     * so they evaluate to the target value.
+     *
+     * Input: num = "123", target = 6
+     * Output: ["1+2+3", "1*2*3"]
+     */
+    public  static List<String> addOperators(String num, int target) {
+        if (num == null || num.length() == 0) {
+            return new ArrayList<>();
+        }
+        List<String> res = new ArrayList<>();
+        dfs(res, num, target, "", 0, 0, 0);
+        return res;
+    }
+
+    public static void dfs(List<String> res, String num, int target, String path, long calcVal, long preNum, int index) {
+        if (index == num.length()) {
+            if (calcVal == target) {
+                res.add(path);
+                return;
+            }
+        }
+
+        for (int i = index; i < num.length(); i++) {
+            if (i != index && num.charAt(index) == '0') {
+                break;
+            }
+            long curNum = Long.parseLong(num.substring(index, i + 1));
+            if (index == 0) {
+                dfs(res, num, target, path + curNum, curNum, curNum, i + 1);
+            } else {
+                dfs(res, num, target, path + "+" + curNum, calcVal + curNum, curNum, i + 1);
+                dfs(res, num, target, path + "-" + curNum, calcVal - curNum, -curNum, i + 1);
+                dfs(res, num, target, path + "*" + curNum, calcVal - preNum + preNum * curNum, preNum * curNum, i + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addOperators("123", 6));
+    }
+
+}
