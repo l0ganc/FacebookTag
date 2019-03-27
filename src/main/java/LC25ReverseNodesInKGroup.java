1476d151c5b1c858b99567b09b340ddab2857e6b
l0ganc <jingyige@usc.edu>
2019-03-27 15:52:47 -0700

    LC25 Reverse Nodes in K group

---
+++ src/main/java/LC25ReverseNodesInKGroup.java
@@ -0,0 +1,40 @@
+public class LC25ReverseNodesInKGroup {
+    /**
+     *Given this linked list: 1->2->3->4->5
+     *
+     * For k = 2, you should return: 2->1->4->3->5
+     *
+     * For k = 3, you should return: 3->2->1->4->5
+     */
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode cur = head;
+        int count = 0;
+
+        while (cur != null && count != k) {
+            cur = cur.next;
+            count++;
+        }
+
+        if (count == k) {
+            cur = reverseKGroup(cur, k);
+            while (count-- > 0) {
+                ListNode next = head.next;
+                head.next = cur;
+                cur = head;
+                head = next;
+            }
+            head = cur;
+        }
+        return head;
+    }
+}
