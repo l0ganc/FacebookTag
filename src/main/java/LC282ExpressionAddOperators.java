e847f7a787adb5fa35d5ad874f7dd81474a81436
l0ganc <jingyige@usc.edu>
2019-04-06 12:24:19 -0700

    LC282 Expression Add Operators  时间复杂度分析

--- src/main/java/LC282ExpressionAddOperators.java
+++ src/main/java/LC282ExpressionAddOperators.java
@@ -10,37 +10,77 @@ public class LC282ExpressionAddOperators {
      * Input: num = "123", target = 6
      * Output: ["1+2+3", "1*2*3"]
      */
-    public  static List<String> addOperators(String num, int target) {
-        if (num == null || num.length() == 0) {
+
+
+    /**
+     * When we use dfs to do this question, the most tricky part is that how to deal with multiplication. For every
+     * addition and subtraction, we just directly adding or subtracting the new number. However, for multiplication,
+     * we should multiply current number and previous number firstly, and then add previous previous number.
+     * So we can use a variable preNum to record every previous number in each recursion step. If current recursive
+     * call is trying multiplication, we should use previous calculation value subtract previous number, and then
+     * adding multiplication result between previous number and current number.
+     * */
+
+    /**
+     * from second digit, we can choose
+     *  + , - , * and empty space (in this case, eg. 12 was treated as one number),
+     *  in total four kinds of choices.
+     *  Also, remember there is a for loop inside every call stack, So, for the call stack of length N,
+     * Total time complexity should be N * 4^(N - 1)
+     *
+     * space complexity is : O(N)
+     */
+    public static List<String> addOperators(String num, int target) {
+        if (num.length() == 0) {
             return new ArrayList<>();
         }
-        List<String> res = new ArrayList<>();
-        dfs(res, num, target, "", 0, 0, 0);
-        return res;
+
+        List<String> result = new ArrayList<>();
+        dfs(result, num, target, "", 0, 0, 0);
+        return result;
     }
 
-    public static void dfs(List<String> res, String num, int target, String path, long calcVal, long preNum, int index) {
-        if (index == num.length()) {
+    /**
+     * @param result: result list to store valid expressions
+     * @param num: input num candidates
+     * @param target: input target number
+     * @param expr: current expression string
+     * @param calcVal: current calculation value
+     * @param preNum: previous number, in order to multiply current number if we want to put * between preNum and curNum
+     * @param pos: current index in the input num array
+     * */
+    public void dfs(List<String> result, String num, int target, String expr, long calcVal, long preNum, int pos) {
+        if (pos == num.length()) {
             if (calcVal == target) {
-                res.add(path);
-                return;
+                result.add(expr);
             }
+            return;
         }
 
-        for (int i = index; i < num.length(); i++) {
-            if (i != index && num.charAt(index) == '0') {
+        // start from first index of current position in num string, try all possible length of nums
+        for (int i = pos; i < num.length(); i++) {
+            // corner case: if current position is 0, we can only use it as a single digit number, should be 0
+            // if it is not a single digit number with leading 0, it should be considered as an invalid number
+            if (i != pos && num.charAt(pos) == '0') {
                 break;
             }
-            long curNum = Long.parseLong(num.substring(index, i + 1));
-            if (index == 0) {
-                dfs(res, num, target, path + curNum, curNum, curNum, i + 1);
-            } else {
-                dfs(res, num, target, path + "+" + curNum, calcVal + curNum, curNum, i + 1);
-                dfs(res, num, target, path + "-" + curNum, calcVal - curNum, -curNum, i + 1);
-                dfs(res, num, target, path + "*" + curNum, calcVal - preNum + preNum * curNum, preNum * curNum, i + 1);
+            long curNum = Long.parseLong(num.substring(pos, i + 1));
+
+            // position 0 should be considered individually, since it does not have any operand character before curNum
+            if (pos == 0) {
+                dfs(result, num, target, expr + curNum, curNum, curNum, i + 1);
+            }
+            else {
+                dfs(result, num, target, expr + "+" + curNum, calcVal + curNum, curNum, i + 1);
+                dfs(result, num, target, expr + "-" + curNum, calcVal - curNum, -curNum, i + 1);
+                // trick part: to calculate multiplication, we should subtract previous number, and then add current
+                // multiplication result to the subtraction result
+                dfs(result, num, target, expr + "*" + curNum, calcVal - preNum + preNum * curNum, preNum * curNum, i + 1);
             }
         }
     }
+
+
 
 
 
