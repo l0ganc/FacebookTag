f4b57483db426dde508c7ae8a93f9e30be2cb4b5
l0ganc <jingyige@usc.edu>
2019-04-07 11:15:44 -0700

    LC42 TRalling Rain Water 再理解

--- src/main/java/LC42TrappingRainWater.java
+++ src/main/java/LC42TrappingRainWater.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class LC42TrappingRainWater {
     /**
      * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
@@ -39,7 +41,7 @@ public class LC42TrappingRainWater {
     }
 
 
-    // one scan solution
+    // one scan solution, time = O(n), space = O(1)
     public static int trap2(int[] height) {
         int res = 0;
         int left = 0;
@@ -58,9 +60,53 @@ public class LC42TrappingRainWater {
         return res;
     }
 
+
+    // stack solution, time = O(n), space = O(1)
+    public static int trap3(int[] height) {
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] >= height[stack.peek()]) {
+                int cur = height[stack.pop()];
+                if (!stack.isEmpty()) {
+                    res += (i - stack.peek() - 1) * (Math.min(height[i], height[stack.peek()]) - cur);
+                }
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
+
+    // new two pointer, time = O(n), space = O(1),想想木桶原理
+    public static int trap4(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int res = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                res += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                res += rightMax - height[right];
+                right--;
+            }
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
         int[] nums = {0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println(trap(nums));
         System.out.println(trap2(nums));
+        System.out.println(trap3(nums));
+        System.out.println(trap4(nums));
     }
 }
