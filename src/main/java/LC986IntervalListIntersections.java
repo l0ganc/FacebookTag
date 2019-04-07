c4434f56e73fb760d9d39fd6fb586e67bbe254ad
l0ganc <jingyige@usc.edu>
2019-04-07 16:13:42 -0700

    LC986 Interval List Intersections add more test cases

--- src/main/java/LC986IntervalListIntersections.java
+++ src/main/java/LC986IntervalListIntersections.java
@@ -31,12 +31,12 @@ public class LC986IntervalListIntersections {
 
         while (i < A.length && j < B.length) {
             int start = Math.max(A[i].start, B[j].start);
-            int end = Math.min(A[i].end, B[i].end);
+            int end = Math.min(A[i].end, B[j].end);
             if (start <= end) {
                 res.add(new Interval(start, end));
             }
 
-            if (A[i].end < B[i].end) {
+            if (A[i].end < B[j].end) {
                 i++;
             } else {
                 j++;
@@ -46,6 +46,38 @@ public class LC986IntervalListIntersections {
     }
 
     public static void main(String[] args) {
+        Interval[] A = new Interval[4];
+        Interval[] B = new Interval[4];
+        A[0] = new Interval(0, 2);
+        A[1] = new Interval(5, 10);
+        A[2] = new Interval(13, 23);
+        A[3] = new Interval(24, 25);
+
+        B[0] = new Interval(1, 5);
+        B[1] = new Interval(8, 12);
+        B[2] = new Interval(15, 24);
+        B[3] = new Interval(25, 26);
+
+
+        Interval[] C = new Interval[1];
+        Interval[] D = new Interval[3];
+        C[0] = new Interval(2, 8);
+
+        D[0] = new Interval(2, 4);
+        D[1] = new Interval(5, 6);
+        D[2] = new Interval(7, 9);
+
+        Interval[] res = intervalIntersection(A, B);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i].start + "," + res[i].end);
+        }
+
+        System.out.println("**********************");
+        System.out.println("");
+        Interval[] res2 = intervalIntersection(C, D);
+        for (int i = 0; i < res2.length; i++) {
+            System.out.println(res2[i].start + "," + res2[i].end);
+        }
 
     }
 
