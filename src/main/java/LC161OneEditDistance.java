20a878800756dfdec134b9edc2e2a621c315f25b
l0ganc <jingyige@usc.edu>
2019-04-07 17:57:28 -0700

    LC161 One Edit Distance 重写

--- src/main/java/LC161OneEditDistance.java
+++ src/main/java/LC161OneEditDistance.java
@@ -1,5 +1,6 @@
 public class LC161OneEditDistance {
     /**
+     * determine if both s and t are one edit distance
     * There're 3 possibilities to satisfy one edit distance apart:
     *
     * 1) Replace 1 char:
@@ -29,6 +30,23 @@ public class LC161OneEditDistance {
         }
 
         //All previous chars are the same, the only possibility is deleting the end char in the longer one of s and t
+        return Math.abs(s.length() - t.length()) == 1;
+    }
+
+
+    // new
+    public static boolean isOneEditDistance2(String s, String t) {
+        for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
+            if (s.charAt(i) != t.charAt(i)) {
+                if (s.length() == t.length()) {
+                    return s.substring(i + 1).equals(t.substring(i + 1));
+                } else if (s.length() < t.length()) {
+                    return s.substring(i).equals(t.substring(i + 1));
+                } else {
+                    return t.substring(i).equals(s.substring(i + 1));
+                }
+            }
+        }
         return Math.abs(s.length() - t.length()) == 1;
     }
 
