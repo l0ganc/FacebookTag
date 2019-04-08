8afb5dfc6d5215f6977b2502705a976dcc831f87
l0ganc <jingyige@usc.edu>
2019-04-08 15:24:44 -0700

    LC114 Flatten Binary Tree To LinkedList

---
+++ src/main/java/LC114FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,62 @@
+import java.util.Stack;
+
+public class LC114FlattenBinaryTreetoLinkedList {
+    /**
+     * Given a binary tree, flatten it to a linked list in-place.
+     *
+     * For example, given the following tree:
+     *
+     *     1
+     *    / \
+     *   2   5
+     *  / \   \
+     * 3   4   6
+     * The flattened tree should look like:
+     *
+     * 1
+     *  \
+     *   2
+     *    \
+     *     3
+     *      \
+     *       4
+     *        \
+     *         5
+     *          \
+     *           6
+     */
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    };
+
+    // stack做
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+
+            if (!stack.isEmpty()) {
+                cur.right = stack.peek();
+            }
+            cur.left = null;
+        }
+    }
+
+}
