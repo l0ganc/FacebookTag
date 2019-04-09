b1757e1fa359cadab5a6d298621c99f79d49677c
l0ganc <jingyige@usc.edu>
2019-04-09 16:55:13 -0700

    Basic Calculator I and II

--- src/main/java/LC227BasicCalculatorII.java
+++ src/main/java/LC227BasicCalculatorII.java
@@ -24,49 +24,135 @@ public class LC227BasicCalculatorII {
      *
      */
 
+    // time = O(n), space = O(n)
     public static int calculate(String s) {
         // Write your code here
-        int len;
-        if(s == null || (len = s.length()) == 0) {
-            return 0;
-        }
-        Stack<Integer> stack = new Stack<Integer>();
         int num = 0;
-        char sign = '+';
-        for(int i = 0; i < len; i ++) {
+        char prevSign = '+';
+        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
             if (Character.isDigit(s.charAt(i))) {
-                num = num * 10 + s.charAt(i) - '0';
-            }
-            if ((!Character.isDigit(s.charAt(i)) && ' ' != s.charAt(i)) || i == len - 1) {
-                if(sign == '-') {
-                    stack.push(-num);
+                num = s.charAt(i) - '0';
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    num = num * 10 + s.charAt(i + 1) -'0';
+                    i++;
                 }
-                if(sign == '+') {
+            }
+
+            if (!Character.isDigit(i) && s.charAt(i) != ' ' || i == s.length() - 1) {
+                if (prevSign == '+') {
                     stack.push(num);
                 }
-                if(sign == '*') {
+                if (prevSign == '-') {
+                    stack.push(-num);
+                }
+                if (prevSign == '*') {
                     stack.push(stack.pop() * num);
                 }
-                if(sign == '/') {
+                if (prevSign == '/') {
                     stack.push(stack.pop() / num);
                 }
-                sign = s.charAt(i);
+                prevSign = s.charAt(i);
                 num = 0;
             }
         }
 
-        int re = 0;
         for (int i : stack) {
-            re += i;
+            res += i;
         }
-        return re;
+        return res;
     }
 
+
+    // 不用stack，O(n) time and O(1) space
+    public static int calculate2(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        s = s.trim();
+        long prevNum = 0;
+        int sum = 0;
+        char prevSign = '+';
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') continue;
+            if (Character.isDigit(c)) {
+                int val = c - '0';
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    val = val * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+
+                if (prevSign == '+') {
+                    sum += prevNum;
+                    prevNum = val;
+                } else if (prevSign == '-') {
+                    sum += prevNum;
+                    prevNum = -val;
+                } else if (prevSign == '*') {
+                    prevNum = prevNum * val;
+                } else if (prevSign == '/') {
+                    prevNum = prevNum / val;
+                }
+            } else {
+                prevSign = c;
+            }
+        }
+
+        sum += prevNum;
+        return sum;
+    }
+
+
+
+    // fb常考题，简化版没有空格，只有数字，'+', '*', 要求O(1)空间
+    public static int calculate3(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        long prevNum = 0;
+        int sum = 0;
+        char prevOp = '+';
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int val = c - '0';
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    val = val * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+
+                if (prevOp == '+') {
+                    sum += prevNum;
+                    prevNum = val;
+                } else if (prevOp == '*') {
+                    prevNum = prevNum * val;
+                }
+            } else {
+                prevOp = c;
+            }
+        }
+        sum += prevNum;
+        return sum;
+    }
 
     public static void main(String[] args) {
         System.out.println(calculate("3+2*2"));
         System.out.println(calculate("3/2"));
         System.out.println(calculate("3+5 / 2"));
+        System.out.println(calculate2("3+2*2"));
+        System.out.println(calculate2("3/2"));
+        System.out.println(calculate2("3+5 / 2"));
+        System.out.println(calculate2("3 * 2 + 4 - 5 / 2"));
+        System.out.println(calculate2(" 3+5 / 2 "));
+        System.out.println("       隔开        ");
+        System.out.println(calculate3("3+2*2"));
+        System.out.println(calculate3("3+2*2+4+3+3*3*3+2"));
     }
 }
 
