f90b7fbb8cbacab1622920331120cf1ac6be4b9c
l0ganc <jingyige@usc.edu>
2019-04-10 10:15:06 -0700

    LC189 Rotated Array

---
+++ src/main/java/LC189RotateArray.java
@@ -0,0 +1,31 @@
+public class LC189RotateArray {
+    /**
+     * Input: [1,2,3,4,5,6,7] and k = 3
+     * Output: [5,6,7,1,2,3,4]
+     * Explanation:
+     * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+     * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+     * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+     */
+
+    // 首先全部反转，然后反转0~k-1, 最后反转k~nums.length - 1
+    // time = O(n)
+    // space = O(1)
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
