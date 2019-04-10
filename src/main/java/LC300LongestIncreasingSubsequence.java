85247cfd45832f368954f4cbc953f35c41dbb37c
l0ganc <jingyige@usc.edu>
2019-04-09 20:04:26 -0700

    LC300 Longest Increasing Subsequence

--- src/main/java/LC300LongestIncreasingSubsequence.java
+++ src/main/java/LC300LongestIncreasingSubsequence.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class LC300LongestIncreasingSubsequence {
     /**
      * Given an unsorted array of integers, find the length of longest increasing subsequence.
@@ -37,8 +39,76 @@ public class LC300LongestIncreasingSubsequence {
         return res;
     }
 
+    // dp 理解版, time = O(n^2), space = O(n)
+    public static int lengthOfLIS2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int res = 1;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    res = Math.max(res, dp[i]);
+                }
+            }
+        }
+        return res;
+    }
+
+    // time = O(nlogn), space = O(n)， using binary search
+    // https://yanjia.me/zh/2018/11/05/70/
+
+    /**
+     *
+     */
+    public static int lengthOfLIS3(int[] nums) {
+        // write your code here
+        if(nums.length == 0){
+            return 0;
+        }
+        // len表示当前最长的升序序列长度（为了方便操作tails我们减1）
+        int len = 0;
+        // tails[i]表示长度为i的升序序列其末尾的数字
+        int[] tails = new int[nums.length];
+        tails[0] = nums[0];
+        // 根据三种情况更新不同升序序列的集合
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] < tails[0]){
+                tails[0] = nums[i];
+            } else if (nums[i] >= tails[len]){
+                tails[++len] = nums[i];
+            } else {
+                // 如果在中间，则二分搜索
+                tails[binarySearch(tails, 0, len, nums[i])] = nums[i];
+            }
+        }
+        return len + 1;
+    }
+    private static int binarySearch(int[] tails, int min, int max, int target){
+        while(min <= max){
+            int mid = min + (max - min) / 2;
+            if(tails[mid] == target){
+                return mid;
+            }
+            if(tails[mid] < target){
+                min = mid + 1;
+            }
+            if(tails[mid] > target){
+                max = mid - 1;
+            }
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
         int[] nums = {10,9,2,5,3,7,101,18};
         System.out.println(lengthOfLIS(nums));
+        System.out.println(lengthOfLIS2(nums));
+        System.out.println(lengthOfLIS3(nums));
     }
 }
