050b496d111559857fa85bd6134eff31daf99467
l0ganc <jingyige@usc.edu>
2019-04-10 20:36:08 -0700

    LC240 Search 2D Matrix II

---
+++ src/main/java/LC240Searcha2DMatrixII.java
@@ -0,0 +1,40 @@
+public class LC240Searcha2DMatrixII {
+    /**
+     *
+     Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+     Integers in each row are sorted in ascending from left to right.
+     Integers in each column are sorted in ascending from top to bottom.
+     Example:
+
+     Consider the following matrix:
+
+     [
+     [1,   4,  7, 11, 15],
+     [2,   5,  8, 12, 19],
+     [3,   6,  9, 16, 22],
+     [10, 13, 14, 17, 24],
+     [18, 21, 23, 26, 30]
+     ]
+     Given target = 5, return true.
+
+     Given target = 20, return false.
+     */
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length - 1;
+        int col = 0;
+
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                row--;
+            } else {
+                col++;
+            }
+        }
+        return false;
+
+    }
+}
