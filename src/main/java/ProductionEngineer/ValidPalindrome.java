b8977c6e1fbe2ab06a5e546e15a211e756b54bc8
l0ganc <jingyige@usc.edu>
2019-06-04 21:34:52 -0700

    Valid Palindrome

---
+++ src/main/java/ProductionEngineer/ValidPalindrome.java
@@ -0,0 +1,45 @@
+package ProductionEngineer;
+
+public class ValidPalindrome {
+    /**
+     * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+     *
+     * Note: For the purpose of this problem, we define empty string as valid palindrome.
+     *
+     * Example 1:
+     *
+     * Input: "A man, a plan, a canal: Panama"
+     * Output: true
+     *
+     * time = O(n)
+     * space = O(1)
+     */
+
+    public static boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        s = s.toLowerCase();
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+}
